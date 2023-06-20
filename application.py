804bd99b1e9cf5bc7efa403ffd4f89820809dba5
uniqname <uniqname@umich.edu>
2023-06-20 12:12:52 -0400

    improved collaborative filtering model

--- application.py
+++ application.py
@@ -34,10 +34,15 @@ data = Dataset.load_from_df(data_df[['user_id', 'movie_id', 'rating']], reader)
 trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
 
 # Define and train the user-based collaborative filtering model
+# model = KNNBasic(sim_options={'user_based': True})
+# model.fit(trainset)
+
+# Train the model with the updated data
+trainset = data.build_full_trainset()
 model = KNNBasic(sim_options={'user_based': True})
 model.fit(trainset)
 
-def get_user_recommendations_collabrative(age, data_df):
+def get_user_recommendations_collabrative(age, gender, occupation, rating, data_df):
 
     #create new user id after all the existing user
     new_user_id = data_df['user_id'].max() + 1
@@ -46,7 +51,11 @@ def get_user_recommendations_collabrative(age, data_df):
             'age': [age] })
 
     new_user_ratings = pd.DataFrame({'user_id': [new_user_id],
-                                     'age': [age] })
+                                     'age': [age],
+                                     'gender': [gender],
+                                     'occupation': [occupation],
+                                     'movie_id': [313],
+                                     'rating': [rating] })
     
         
     updated_data_df = pd.concat([data_df, new_user_ratings], ignore_index=True)
@@ -54,25 +63,31 @@ def get_user_recommendations_collabrative(age, data_df):
     # Create a Surprise Dataset from the updated DataFrame
     updated_data = Dataset.load_from_df(updated_data_df[['user_id', 'movie_id', 'rating']], reader)
 
-    # Train the model with the updated data
+    trainset_new = updated_data.build_full_trainset()
+
+    # Use the existing model for predictions
     trainset_new = updated_data.build_full_trainset()
     model_new = KNNBasic(sim_options={'user_based': True})
     model_new.fit(trainset_new)
+    # model_new.train(trainset_new)
 
     predictions = [model_new.predict(new_user_id, movie_id).est for movie_id in updated_data_df['movie_id']]
 
     # Sort the predictions by predicted rating in descending order
     sorted_predictions = sorted(zip(updated_data_df['movie_id'], predictions), key=lambda x: x[1], reverse=True)
-    
+
     # Get the top 5 recommended movie IDs
     # recommended_movies = ','.join([str(movie_id) for movie_id, _ in sorted_predictions[:5]])
+    recommended_movies = []
+    for movie_id, _ in sorted_predictions:
+        if movie_id != 313 and movie_id not in recommended_movies:
+            recommended_movies.append(movie_id)
+            if len(recommended_movies) == 5:
+                break
 
-    recommended_movies = sorted_predictions[:5]
-
-    # Get the movie titles and IMDB URLs for the recommended movie IDs
     recommended_movies_info = []
-    for movie_id, _ in recommended_movies:
-        movie_info = data_df[data_df['movie_id'] == movie_id][['movie_id','title', 'IMDb_URL']].iloc[0]
+    for movie_id in recommended_movies:
+        movie_info = data_df[data_df['movie_id'] == movie_id][['movie_id', 'title', 'IMDb_URL']].iloc[0]
         recommended_movies_info.append((movie_info['movie_id'], movie_info['title'], movie_info['IMDb_URL']))
 
     return recommended_movies_info
@@ -95,11 +110,11 @@ def index():
     if request.method == 'POST':
         # Retrieve input data from the request form
         name = request.form.get('name')
-        age = request.form.get('age')
+        age = int(request.form.get('age'))
         email = request.form.get('email')
         gender = request.form.get('gender')
-        city = request.form.get('city')
-        country = request.form.get('country')
+        occupation = request.form.get('occupation')
+        rating = int(request.form.get('rating'))
 
         # Validate email address
         email_pattern = r'^[a-zA-Z0-9_.+-]+@gmail\.com$'
@@ -110,29 +125,10 @@ def index():
         if int(age) < 18:
             error_message = "You must be at least 18 years old."
             return render_template("./form.html", error=error_message)
-        
-        # conn = sqlite3.connect('new_user_database.db')
-        # cursor = conn.cursor()
 
-        # cursor.execute('''CREATE TABLE IF NOT EXISTS users
-        #                 (name TEXT, age INTEGER)''')
-
-
-        # cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))
-
-        # conn.commit()
-        # conn.close()
-
-        # conn = sqlite3.connect('new_user_database.db')
-        # cursor = conn.cursor()
-        # cursor.execute("SELECT * FROM users WHERE name = ? AND age = ?", (name, age))
-        # user = cursor.fetchone()
-
-        recommendations = get_user_recommendations_collabrative(age, data_df)
+        recommendations = get_user_recommendations_collabrative(age, gender, occupation, rating, data_df)
         if recommendations:
             return response(recommendations)
-
-
 
     # If it's a GET request, render an HTML form for input
     return render_template("./form.html")
